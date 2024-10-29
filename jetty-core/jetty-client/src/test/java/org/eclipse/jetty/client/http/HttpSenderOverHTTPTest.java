89bf780d09ab561d4f037afce0e7f376b131813c
Sheldon <shaoxt@gmail.com>
2024-10-29 13:46:52 -0700

    Avoid unnecessary change

--- jetty-core/jetty-client/src/test/java/org/eclipse/jetty/client/http/HttpSenderOverHTTPTest.java
+++ jetty-core/jetty-client/src/test/java/org/eclipse/jetty/client/http/HttpSenderOverHTTPTest.java
@@ -43,7 +43,6 @@ import org.junit.jupiter.api.Test;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.wildfly.common.Assert.assertFalse;
 
 public class HttpSenderOverHTTPTest
 {
